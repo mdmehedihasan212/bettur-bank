6cc2d4a2c4b60163bc043c3e2e54be88c602fc41
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-02-12 22:38:16 +0600

    function run

--- js/function-sestem.js
+++ js/function-sestem.js
@@ -1,19 +1,8 @@
-function getDepositeValue(inputId) {
-    const depositeInput = document.getElementById(inputId);
+document.getElementById('deposite-button').addEventListener('click', function () {
+    const depositeInput = document.getElementById('deposite-input');
     const depositeAmountText = depositeInput.value;
     const depositeAmount = parseFloat(depositeAmountText);
-    // clear input value
-    depositeInput.value = '';
-    return depositeAmount;
-};
-
-document.getElementById('deposite-button').addEventListener('click', function () {
-    // const depositeInput = document.getElementById('deposite-input');
-    // const depositeAmountText = depositeInput.value;
-    // const depositeAmount = parseFloat(depositeAmountText);
     // console.log(depositeAmount);
-
-    const depositeAmount = getDepositeValue('deposite-input');
 
     // get input value add
     const depositeTotal = document.getElementById('deposite-total');
@@ -23,15 +12,14 @@ document.getElementById('deposite-button').addEventListener('click', function ()
     // depositeTotal.innerText = depositeAmountText;
     depositeTotal.innerText = previousDeposite + depositeAmount;
 
+    // clear input value
+    depositeInput.value = '';
 
     // add balance
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
     balanceTotal.innerText = previousBalanceTotal + depositeAmount;
-
-    // clear input value
-    depositeInput.value = '';
 
 })
 
